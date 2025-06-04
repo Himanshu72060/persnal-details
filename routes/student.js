cabc324f55a3caeeb76518332875ccec1d35475b
Himanshu72060 <himanshusharma00898@gmail.com>
2025-06-04 11:20:16 +0530

    new code

--- routes/student.js
+++ routes/student.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const Student = require('../models/Student');
 const path = require('path');
 
-// all code
 
 // Multer setup
 const storage = multer.diskStorage({
